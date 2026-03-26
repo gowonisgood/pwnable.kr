0f3a303a39f3191aa8f6b29a9a3b6a828aed575e
gOGo oNe <144209770+gowonisgood@users.noreply.github.com>
2026-03-26 15:15:54 +0900

    Add files via upload

---
+++ random/random.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	unsigned int rand = 0x6b8b4567;
+	unsigned int key = rand ^ 0xcafebabe;
+
+	printf("%0x\n", key);
+	printf("%0d\n", key);
+
+	printf("%0x\n", key ^ rand); //for check
+
+
+}
